8ae5240247f9d71e1d7162baa35ba09b5640fb0b
Javier <43155459+javierchb@users.noreply.github.com>
2020-05-04 09:34:34 -0500

    Add files via upload

---
+++ includes/navBarUsuario.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a class="navbar-brand" href="#"> PHP CRUD BLOG</a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="VpostUsuariosSesion.php"> Posts </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="VperfilUsuario.php"> Perfil </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Vusuario.php"> Menú </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php"> Cerrar Sesión </a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ includes/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a class="navbar-brand" href="#"> PHP CRUD BLOG</a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php"> Inicio </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Vregistro.php"> Registro </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="VinicioSesion.php"> Iniciar Sesión </a>
+            </li>
+        </ul>
+    </div>
+</nav>
